be44f26fcb5f054c7a6a9ddabce918e6f8ca042f
Newbie465 <chiragbhatnagar465@gmail.com>
2022-04-22 15:10:31 +0530

    Merge branch 'main' of https://github.com/Newbie465/capstone-jobportal

--- client/src/pages/JobInfo.js
+++ client/src/pages/JobInfo.js
@@ -1,14 +1,27 @@
 import React from 'react'
 import DefaultLayout from '../components/DefaultLayout'
 import { useParams } from 'react-router-dom'
+import { useSelector, useDispatch} from 'react-redux'
 
 function JobInfo({ match }) {
   const { id } = useParams()
+  const { jobs } = useSelector(state=>state.jobsReducer)
+
+  const job = jobs.find(job=>job._id == id)
   return (
     <div>
         <DefaultLayout>
-            <h1>Job Info</h1>
-            { id }
+            <h1><b>Job Info</b></h1>
+            <div className = "JobInfo">
+              <h1><b>{job.title}</b></h1>
+              <p>{job.company}</p>
+              <p>{job.fullDescription}</p>
+              <h4><b>Skills</b></h4>
+              <p>{job.skillsRequired}</p>
+              <h4><b>Minimum Qualifications</b></h4>
+              <p>{job.minimumQualification}</p>
+
+            </div>
         </DefaultLayout>
     </div>
   )
